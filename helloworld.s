9fb7b4a9bc01a8a609c04ef003f77e3eee53e195
adob71 <adob71@gmail.com>
2023-02-15 16:26:24 +0100

    commit

--- helloworld.s
+++ helloworld.s
@@ -1,5 +1,5 @@
-// assembler program to print helloworld to stdout
-// X0,X1,X2 - parameter for unix system call
+// assembler program to print string to stdout
+// X0,X1,X2 - parametears for unix system calls
 // X16 - unix system call #
 
 .global _start  // start address for linker
@@ -7,14 +7,14 @@
 
 _start: 
 
-// Setup the parameters to print helloworld and then call the Kernel to do it
+// setup parameters to print string and call kernel to do it
 mov X0, #1      // stdout = 1
 adr X1, string  // string to print
 mov X2, #11     // length of the string
 mov X16, #4     // unix system call #4 is to write the string
 svc #0x80       // call kernel to write the string
 
-// Setup the parameters to exit the program and then call the kernel to do it
+// setup parameters to exit the program and call kernel to do it
 mov X0, #0      // return code = 0
 mov X16, #1     // unix system call #1 is to exit the program
 svc #0x80       // call kernel to exit the program
