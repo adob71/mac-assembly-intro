9498fe4814032b981ce843ace56f4f9c7d9ec0c6
adob71 <adob71@gmail.com>
2023-02-15 15:50:25 +0100

    commit

--- helloworld.s
+++ helloworld.s
@@ -8,7 +8,7 @@
 _start: 
 
 // Setup the parameters to print helloworld and then call the Kernel to do it
-mov X0, #1      // 1 = stdout
+mov X0, #1      // stdout = 1
 adr X1, string  // string to print
 mov X2, #11     // length of the string
 mov X16, #4     // unix system call #4 is to write the string
@@ -16,7 +16,7 @@ svc #0x80       // call kernel to write the string
 
 // Setup the parameters to exit the program and then call the kernel to do it
 mov X0, #0      // return code = 0
-mov X16, #1     // unix system call # 1 is to exit the program
+mov X16, #1     // unix system call #1 is to exit the program
 svc #0x80       // call kernel to exit the program
 
 string: 
